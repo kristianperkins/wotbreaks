d227e13376a6e742518d59b5f9bdedcf2d0a0983
Kristian Perkins <kperkins@wotifgroup.com>
2014-12-09 15:05:58 +1000

    all bonuses all the time

--- superwotifbreakout.js
+++ superwotifbreakout.js
@@ -93,6 +93,8 @@ function Ball(id, velocity) {
                 if ($b.hasClass("hotdeal")) {
                     $b.removeClass("hotdeal");
                 } else {
+                    var bonus = $("<div class='breakout-bonus' style='width: 70px; height: 50px; left: " + curr.x + "px; top:"+ curr.y  + "px; position: absolute; border-radius: 20%; background-image: linear-gradient(to bottom, #f5f9fc, #d1dbe4);'>stuff</div>");
+                    bonus.appendTo("body");
                     // add to score
                     score += +$b.text();
                     $(".breakout-score b").text(score);
@@ -109,6 +111,7 @@ function Ball(id, velocity) {
         this.collideWindow(this.pos, nextPos);
         this.collideBlocks(this.pos, nextPos);
         this.collidePaddle(this.pos, nextPos);
+        $('.breakout-bonus').css('top', '+=1');
         this.pos = nextPos;
         this.dom.offset({
             left: this.pos.x,
@@ -116,132 +119,6 @@ function Ball(id, velocity) {
         });
     };
 }
-
-/*
-function Ball(id, velocity) {
-    this.speed = 4;
-    this.v = velocity;
-    this.dom = $(id);
-    this.update = function() {
-        // TODO: bonds and collisions?
-        this.collide();
-        var pos = this.dom.position();
-        var scrollTop = $(document).scrollTop();
-        var topBound = $('table.matrix-content').offset().top;
-        var someTr = $('tr.grid-header').last();
-        var leftBound = someTr.offset().left;
-        var rightBound = leftBound + someTr.width();
-        if (pos.top <= topBound) {
-            this.v.y = 1;
-        } else if (pos.top >= $(window).height() + scrollTop) {
-            console.log('woe to the fallen');
-            this.v.y = -1;
-        }
-        if (pos.left <= leftBound) {
-            this.v.x = 1;
-        } else if (pos.left >= rightBound) {
-            this.v.x = -1;
-        }
-        pos.left += (this.v.x * this.speed);
-        pos.top += (this.v.y * this.speed);
-        this.dom.offset(pos);
-    };
-    this.collisionPoint = function(div) {
-        var d = $(div);
-        var pos = d.offset();
-        var w = d.width(), h = d.height();
-        if (this.v.x > 0 ) {
-            if (this.v.y > 0) {
-                console.log("top left");
-                return new Point(pos.left, pos.top);
-            } else {
-                console.log("bottom left");
-                return new Point(pos.left, pos.top + h);
-            }
-        } else {
-            if (this.v.y > 0) {
-                console.log("top right");
-                return new Point(pos.left + w, pos.top);
-            } else {
-                console.log("bottom right");
-                return new Point(pos.left + w, pos.top + h);
-            }
-        }
-    };
-    this.collide = function() {
-        var selector = 'div';
-        var self = this;
-        var bp = this.dom.position();
-        var changeX = false;
-        var changeY = false;
-        self.dom.collision('tr.deals:visible').each(function(a, b) {
-            var trId = '#' + b.id;
-            var collided = false;
-            self.dom.collision(trId + ' td.weekday, ' + trId + ' td.weekend').each(function(idx, elem) {
-                $elem = $(elem);
-                collided = true;
-                if (!elem.hit) {
-                    // ball response
-                    var p = self.collisionPoint(elem);
-                    console.log("collision corner: ", p);
-                    console.log("bp: ", bp);
-                    console.log("x difference", Math.abs(p.x - bp.left + 10));
-                    console.log("y difference", Math.abs(p.y - bp.top + 10));
-                    console.log("offsetWidth", elem.offsetWidth);
-                    console.log("offsetHeight", elem.offsetHeight);
-                    if (p.x < bp.left + 10 && bp.left + 10 < p.x + elem.offsetWidth) {
-                        console.log("changing x within x..");
-                        changeX = true;
-                    } else if (p.y < bp.top + 10 && bp.top + 10 < p.y + elem.offsetHeight) {
-                        console.log("changing y within y..");
-                        changeY = true;
-                    } else if (Math.abs(p.x - bp.left + 10) > Math.abs(p.y - bp.top + 10)) {
-                        if (!changeY) {
-                            console.log("changing y direction..");
-                            changeY = true;
-                        }
-                    } else { 
-                        if (!changeX ) {
-                            console.log("changing x direction..");
-                            changeX = true;
-                        }
-                    }
-                    if ($elem.hasClass("hotdeal")) {
-                        $elem.removeClass("hotdeal");
-                    } else {
-                        // add to score
-                        score += +$(elem).text();
-                        $(".breakout-score b").text(score);
-                        elem.hit = true;
-                        $elem.attr('class', 'anim sold').html('SOLD');
-                    }
-                    console.log(elem);
-                    var pos = $(elem).position();
-                }
-                if (debugStep && !confirm("hit")) {
-                  running = false;
-                } 
-                
-            });
-            if (changeX) {
-                self.v.x *= -1;
-                console.log("changing x ", self.v.x);
-            }
-            if (changeY) {
-                self.v.y *= -1;
-                console.log("changing y ", self.v.y);
-            }
-            if ($('tr.deals:visible td:not(.sold)').not('.summary').length == 0) {
-                nextLevel();
-            }
-        });
-        this.dom.collision('#paddle').each(function(idx, elem) {
-            //self.v.x = - self.v.x;
-            self.v.y = - self.v.y;
-        });
-    };
-}
-*/
 
 function main() {
     window.broken = true;
@@ -272,7 +149,7 @@ function main() {
         });
 		$("<div id='paddle' style='background:pink; width: 200px; height:1em; left:15px; bottom:15px; position: fixed;z-index=1000000;'></div>").appendTo("body");
 		$("<div id='ball' class='ball' style='width: 20px; height:20px; top:15px; bottom:15px; position: fixed; z-index=1000000; border-radius: 50%; margin: 0; background: radial-gradient(circle at 7px 7px, #CCC, #000);'></div>").appendTo("body");
-        $("<div id='level-head-div' style='display: none; top: 250px; width: 100%; height: 40px; position: absolute; background-image: linear-gradient(to bottom, #f5f9fc, #d1dbe4); vertical-align: middle; text-align: centre'><h1 id='level-heading' style='text-align: center; margin: auto; padding-top: 5px;'>Level X</h1></div>").appendTo("body");
+        $("<div id='level-head-div' style='display: none; top: 260px; width: 100%; height: 40px; position: absolute; background-image: linear-gradient(to bottom, #f5f9fc, #d1dbe4); vertical-align: middle; text-align: centre'><h1 id='level-heading' style='text-align: center; margin: auto; padding-top: 5px;'>Level X</h1></div>").appendTo("body");
         init = true;
         var m = $('#main');
         var startPos = {
